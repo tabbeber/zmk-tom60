1e3687de689367fe258506f3e9bf6f82b6032c2a
tabbeber <taberstad@gmail.com>
2021-12-20 15:18:57 +0100

    new setup

---
+++ app/boards/arm/tom60/board.cmake
@@ -0,0 +1,6 @@
+# BSD 2-Clause "Simplified" License
+
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
